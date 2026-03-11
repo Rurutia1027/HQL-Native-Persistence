747e23aae6e7840d7ad28610a42954f65c6e1ada
Rurutia1027 <rurutia1027@gmail.com>
2026-03-12 00:23:29 +0800

    add docker compose for tidb and sharding spring applicaiton example app class

---
+++ examples/persistence-sharding-example/src/main/java/org/tus/sharding/example/ShardingExampleApplication.java
@@ -0,0 +1,11 @@
+package org.tus.sharding.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShardingExampleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShardingExampleApplication.class, args);
+    }
+}
